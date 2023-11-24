f1787ca14ce7545cb54b25173655f7920ed05a5a
salidamaharjan <salidamaharjan@outlook.com>
2023-11-24 08:45:25 -0600

    element created and added a question using js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,5 @@
-console.log("hello");
+//getting an element from html and storing in a variable in a DOM
+var questionTitle = document.querySelector(".question-title");
+
+//adding the question to h1 tag
+questionTitle.textContent = "What is your name?";
