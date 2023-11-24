b483a697530c10872434bf34a15c8ecab6f2cbaf
salidamaharjan <salidamaharjan@outlook.com>
2023-11-24 08:58:40 -0600

    added answer option in list using js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
 //getting an element from html and storing in a variable in a DOM
 var questionTitle = document.querySelector(".question-title");
+var answerContainer = document.querySelector(".answer-container");
+var options = document.querySelectorAll(".option");
 
 //adding the question to h1 tag
 questionTitle.textContent = "What is your name?";
+
+//adding answer option to li tag
+options[0].textContent = "Salida";
+options[1].textContent = "Hari";
+options[2].textContent = "Ram";
+options[3].textContent = "Maharjan";
