038271e51f2ea67f40a02b76915db9495f255855
salidamaharjan <salidamaharjan@outlook.com>
2023-11-24 10:06:34 -0600

    created question and answer set

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,21 +3,30 @@ var questionTitle = document.querySelector(".question-title");
 var answerContainer = document.querySelector(".answer-container");
 var options = document.querySelectorAll(".option");
 
-//array of answers created with different option
-var answersArr = ["Salida", "Hari", "Ram", "Maharjan"];
-
-//array of questions
+//array of object containing question and answer
 var questionsArr = [
-  "What is your name?",
-  "Where do you live?",
-  "What is you hobby?",
+  {
+    question: "What is your name?",
+    answer: ["Salida", "Hari", "Ram", "Maharjan"],
+  },
+  {
+    question: "Where do you live?",
+    answer: ["UK", "USA", "France", "Italy"],
+  },
+  {
+    question: "What is you hobby?",
+    answer: ["Paint", "Cooking", "Dancing", "Listening Music"],
+  },
 ];
+//created to change in each click
+var indexOfQuestionsArr = 0;
+
 prepareQuestion();
 
 //question and answer placed in a function to execute when called
 function prepareQuestion() {
-  questionTitle.textContent = questionsArr[0];
-  console.log("questionTitle", questionTitle.textContent);
+  questionTitle.textContent = questionsArr[indexOfQuestionsArr];
+
   //looping the option to assign a different values from answerArr
   for (var i = 0; i < answersArr.length; i++) {
     options[i].textContent = answersArr[i];
@@ -25,4 +34,7 @@ function prepareQuestion() {
 }
 
 //using add event listener to change the question on click
-answerContainer.addEventListener("click", function () {});
+answerContainer.addEventListener("click", function () {
+  indexOfQuestionsArr++;
+  prepareQuestion();
+});
