fa71a3f2900cb3cc45b68f53bd9c82a6f6961769
salidamaharjan <salidamaharjan@outlook.com>
2023-11-24 09:17:31 -0600

    using addEventListener method to change question to index 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,24 @@ var questionTitle = document.querySelector(".question-title");
 var answerContainer = document.querySelector(".answer-container");
 var options = document.querySelectorAll(".option");
 
-//adding the question to h1 tag
-questionTitle.textContent = "What is your name?";
-
 //array of answers created with different option
 var answersArr = ["Salida", "Hari", "Ram", "Maharjan"];
 
+//array of questions
+var questionsArr = [
+  "What is your name?",
+  "Where do you live?",
+  "What is you hobby?",
+];
+
+//adding the question to h1 tag
+questionTitle.textContent = "What is your name?";
 //looping the option to assign a different values from answerArr
 for (var i = 0; i < answersArr.length; i++) {
   options[i].textContent = answersArr[i];
 }
+
+//using add event listener to change the question on click
+answerContainer.addEventListener("click", function () {
+    questionTitle.textContent = questionsArr[1];
+});
